12b3a5ac58dcc6ba2d03a89bb71c5ee79cd249d1
BIT Student <classroom@bgit.rs>
2019-02-12 17:21:33 +0100

    First 5 Tasks

--- switch/exercise1.js
+++ switch/exercise1.js
@@ -66,3 +66,109 @@
 // }
 
 // console.log(result);
+
+// // TASK 3
+
+// var dayInWeek = 10;
+// var result;
+
+// switch (dayInWeek) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "It's weekday";
+//         break;
+//     case 6:
+//     case 7:
+//         result = "It's weekend";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 7";
+//         break;
+// }
+
+// console.log(result);
+
+
+// // TASK 4
+
+// var monthInYear = 13;
+// var result;
+
+// switch (monthInYear) {
+//     case 1:
+//         result = "January";
+//         break;
+//     case 2:
+//         result = "February";
+//         break;
+//     case 3:
+//         result = "March";
+//         break;
+//     case 4:
+//         result = "April";
+//         break;
+//     case 5:
+//         result = "May";
+//         break;
+//     case 6:
+//         result = "June";
+//         break;
+//     case 7:
+//         result = "July";
+//         break;
+//     case 8:
+//         result = "August";
+//         break;
+//     case 9:
+//         result = "September";
+//         break;
+//     case 10:
+//         result = "October";
+//         break;
+//     case 11:
+//         result = "November";
+//         break;
+//     case 12:
+//         result = "December";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 12";
+//         break;
+// }
+
+// console.log(result);
+
+// // TASK 5
+
+// var monthInYear = 13;
+// var result;
+
+// switch (monthInYear) {
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "Spring";
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         result = "Summer";
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         result = "Autumn";
+//         break;
+//     case 12:
+//     case 1:
+//     case 2:
+//         result = "Winter";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 12";
+// }
+
+// console.log(result);
