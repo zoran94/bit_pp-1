2e69e1c609d6db820c4db835a677752a18003727
BIT Student <classroom@bgit.rs>
2019-02-12 17:10:58 +0100

    First 2 Tasks

---
+++ switch/exercise1.js
@@ -0,0 +1,68 @@
+// // TASK 1
+
+// var dayInWeek = 1;
+// var result;
+
+// switch (dayInWeek) {
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+//     default:
+//         result = "That day in a week does not exist!";
+//         break;
+// }
+
+// console.log(result);
+
+
+// // TASK 2
+
+// var dayInWeek = 10;
+// var result;
+
+// switch (dayInWeek) {
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+//     default:
+//         result = "Input must be a number between 1 and 7";
+//         break;
+// }
+
+// console.log(result);
