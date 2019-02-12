c7b741558b2f71f31a9626f3d51036dc9ec7ce20
BIT Student <classroom@bgit.rs>
2019-02-12 17:27:26 +0100

    Finished 6 tasks in first exercise

--- switch/exercise1.js
+++ switch/exercise1.js
@@ -172,3 +172,32 @@
 // }
 
 // console.log(result);
+
+
+// // TASK 6
+
+// var grade = "M";
+// var result;
+
+// switch (grade) {
+//     case "A":
+//         result = "Good job";
+//         break;
+//     case "B":
+//         result = "Pretty good";
+//         break;
+//     case "C":
+//         result = "Passed";
+//         break;
+//     case "D":
+//         result = "Not so good";
+//         break;
+//     case "F":
+//         result = "Failed";
+//         break;
+//     default:
+//         result = "Unknown grade";
+//         break;
+// }
+
+// console.log(result);
