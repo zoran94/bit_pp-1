22713b9f408d66a5db022e16ee3ac6a9439ba80c
BIT Student <classroom@bgit.rs>
2019-02-13 09:21:22 +0100

    First 2 Tasks in Arrays

---
+++ arrays/exercise1.js
@@ -0,0 +1,14 @@
+// // TASK 1
+
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// console.log(months[5]);
+// console.log(months[9]);
+// console.log(months[0]);
+
+
+// // TASK 2
+
+// var daysInWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// console.log(daysInWeek[6]);
