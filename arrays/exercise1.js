36dbba16a4815a76dd82c1ae77e65184060db151
BIT Student <classroom@bgit.rs>
2019-02-13 16:46:24 +0100

    finished all tasks

--- arrays/exercise1.js
+++ arrays/exercise1.js
@@ -11,3 +11,43 @@
 
 // console.log(daysInWeek[6]);
 // console.log(daysInWeek[daysInWeek.length - 1]); 
+
+
+// // TASK 3
+
+// var numbers = [2, -4, 5, -2, -11];
+// var r = '';
+
+// if (numbers[0] < 0) {
+//     r += numbers[0];
+// }
+// if (numbers[1] < 0) {
+//     r += " " + numbers[1];
+// }
+// if (numbers[2] < 0) {
+//     r += " " + numbers[2];
+// }
+// if (numbers[3] < 0) {
+//     r += " " + numbers[3];
+// }
+// if (numbers[4] < 0) {
+//     r += " " + numbers[4];
+// }
+
+// console.log(r);
+
+
+// // TASK 4
+
+// var arr = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// console.log(arr[3]);
+// console.log(arr[6]);
+
+
+// // TASK 5
+
+// var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// console.log(arr[0][3]);
+// console.log(arr[2][1]);
