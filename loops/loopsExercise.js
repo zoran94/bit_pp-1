74cf8c41ae1e2bdcbd01c167289e2704964a0ae3
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-02-14 18:44:14 +0100

    final

--- loops/loopsExercise.js
+++ loops/loopsExercise.js
@@ -100,27 +100,11 @@
 
 // TASK 8
 
-//// One way of understanding this problem
-
-// for (var i = 1; i < 100; i++) {
-//     if (i % 3 === 0) {
-//         console.log("Fizz");
-//     } else if (i % 5 === 0 && !(i % 3 === 0)) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(i);
-//     }
-    
-//     if (i % 5 === 0 && i % 3 === 0) {
-//         console.log("FizzBuzz");
-//     }
-// }
-
-//// The other way
+// //Final solution
 
 // for (var i = 1; i <= 100; i++){
 //     if (i % 3 === 0 && i % 5 === 0) {
-//      console.log("FizzBuzz");
+//      console.log("Fizz, Buzz, FizzBuzz");
 //     } else if (i % 3 === 0) {
 //         console.log("Fizz");
 //     } else if (i % 5 === 0) {
