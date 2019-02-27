9369e9366fd5ef6ec5092c4daa32bccf4fed2941
BIT Student <classroom@bgit.rs>
2019-02-27 14:48:22 +0100

    just started

---
+++ functions/functionsPractical10.js
@@ -0,0 +1,17 @@
+// // TASK 1 - Write IIFE that replaces the first and the last element of the given array and prints out its elements.
+// // Input array: [4, 5, 11, 9]
+// // Output array: [ 9, 5, 11, 4]
+
+// console.log((function () {
+//     var a = [4, 5, 11, 9];
+//     var temp = a[0];
+//     a[0] = a[a.length - 1];
+//     a[a.length - 1] = temp;
+//     return a;
+// })());
+
+
+// TASK 2 - Write IIFE that calculates the surface area of the given rectangle with sides a and b. 
+// Input: 4 5
+// Output: 20 
+
