faea5de459f3cbfcc4424c14572785a8f70f64c3
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-03-03 00:18:14 +0100

    finished

--- functions/functionsPractical3.js
+++ functions/functionsPractical3.js
@@ -131,14 +131,31 @@
 
 
 
-// TASK 7 - Write a function that says whether a number is perfect.
+// // TASK 7 - Write a function that says whether a number is perfect.
 
-// 28 -> 28 is a perfect number.
+// // 28 -> 28 is a perfect number.
 
-//Note: According to Wikipedia: In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself(also known as its aliquot sum).Equivalently, a perfect number is a number that is half the sum of all of its positive divisors(including itself).
+// //Note: According to Wikipedia: In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself(also known as its aliquot sum).Equivalently, a perfect number is a number that is half the sum of all of its positive divisors(including itself).
 
-//E.g.: The first perfect number is 6, because 1, 2 and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: (1 + 2 + 3 + 6) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
+// //E.g.: The first perfect number is 6, because 1, 2 and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: (1 + 2 + 3 + 6) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
 
+
+// function isPerfect(num) {
+//     var divisorsSum = 0;
+//     for (var i = 1; i <= num / 2; i++) {
+//         if (num % i === 0) {
+//             divisorsSum += i;
+//         }
+//     }
+ 
+//     if (divisorsSum === num && divisorsSum !== 0) {
+//         console.log(num + " is a perfect number.");
+//     } else {
+//         console.log(num + " is not a perfect number.");
+//     }
+//  }
+ 
+//  isPerfect(496);
 
 
 // // TASK 8 - Write a function to find a word within a string.
@@ -202,7 +219,36 @@
 
 
 
+// // TASK 10 - Write a program to find the most frequent item of an array.
 
-// TASK 10 - Write a program to find the most frequent item of an array.
+// // [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
 
-// [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
+// function mostFrequent(arr) {
+//     var char = "";
+//     var count = 0;
+//     var maxCount = 0;
+
+//     for (var i = 0; i < arr.length; i++) {
+
+//         if (arr[i] === char) {
+//             continue;
+//         }
+
+//         for (var j = 0; j < arr.length; j++) {
+//             if (arr[i] === arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count >= maxCount) {
+//             maxCount = count;
+//             count = 0;
+//             char = arr[i];
+//         } else {
+//             count = 0;
+//         }
+//     }
+//     return char;
+// }
+
+// console.log(mostFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]));
